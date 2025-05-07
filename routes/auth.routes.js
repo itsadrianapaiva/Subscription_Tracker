e87d584fc5187805c19739c563b383bbadb8c1e3
Adriana Paiva <adrianapaivausa@icloud.com>
2025-05-07 19:15:22 +0100

    models and error handler

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 
+import { signUp } from '../controllers/auth.controller.js';
+
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => res.send( { title: 'Sign up'}));
+authRouter.post('/sign-up', signUp);
 
 authRouter.post('/sign-in', (req, res) => res.send( { title: 'Sign in'}));
 
